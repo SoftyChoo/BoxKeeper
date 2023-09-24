23ae53d6efc7de0fa3a419e445fa697a01365580
추민수 <devsoftychoo@gmail.com>
2023-09-24 18:18:24 +0900

    [Design] : Siren Selector

--- boxkeeper_v0.11/app/src/main/java/com/example/boxkeeper/CommonUtils.java
+++
@@ -1,4 +0,0 @@
-package com.example.boxkeeper;
-
-public class CommonUtils {
-}
--- boxkeeper_v0.11/app/src/main/java/com/example/boxkeeper/MainActivity.java
+++ boxkeeper_v0.11/app/src/main/java/com/example/boxkeeper/MainActivity.java
@@ -13,13 +13,16 @@ import android.view.MenuItem;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 import android.widget.TextView;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+
 import android.view.View;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView Weights;
     private TextView Ab;
     private String changestate;
+    private ImageButton sirenBox1;
+    private ImageButton sirenBox2;
+    private ImageButton sirenBox3;
+    private ImageButton sirenBox4;
 
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
     DatabaseReference state_of_the_box = mRootRef.child("State_of_the_Box");
@@ -43,21 +50,25 @@ public class MainActivity extends AppCompatActivity {
         infobtn = (ImageButton) findViewById(R.id.btn_info);
         Weights = (TextView) findViewById(R.id.tv_box1_plus_minus);
         Ab = (TextView) findViewById(R.id.tv_box1_abs);
+        sirenBox1 = (ImageButton) findViewById(R.id.btn_siren1);
+        sirenBox2 = (ImageButton) findViewById(R.id.btn_siren2);
+        sirenBox3 = (ImageButton) findViewById(R.id.btn_siren3);
+        sirenBox4 = (ImageButton) findViewById(R.id.btn_siren4);
 
-        findViewById(R.id.btn_menu).setOnClickListener(new View.OnClickListener(){
-            public void onClick(final View view){
-                final PopupMenu popupMenu = new PopupMenu(getApplicationContext(),view);
-                getMenuInflater().inflate(R.menu.popup,popupMenu.getMenu());
+        findViewById(R.id.btn_menu).setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View view) {
+                final PopupMenu popupMenu = new PopupMenu(getApplicationContext(), view);
+                getMenuInflater().inflate(R.menu.popup, popupMenu.getMenu());
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem menuItem) {
-                        if(menuItem.getItemId() == R.id.action_menu1){
+                        if (menuItem.getItemId() == R.id.action_menu1) {
                             Intent intent = new Intent(MainActivity.this, UserCommand.class);
                             startActivity(intent);
-                        }else if (menuItem.getItemId() == R.id.action_menu2){
+                        } else if (menuItem.getItemId() == R.id.action_menu2) {
                             Intent intent = new Intent(MainActivity.this, Delivery.class);
                             startActivity(intent);
-                        }else {
+                        } else {
                             Intent intent = new Intent(MainActivity.this, BoxLog.class);
                             startActivity(intent);
                         }
@@ -82,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        initSiren();
 
         state_of_the_box.addValueEventListener(new ValueEventListener() {
             @Override
@@ -93,22 +105,20 @@ public class MainActivity extends AppCompatActivity {
 
                 //Minsu's code : 만약 state값이 -라면 사용자가 볼 수 있게 0으로 값 초기화
                 //-> 값을 측정할 때 값이 튈 때 -값이 나오는 문제를 해결하기 위한 코드
-                if(state.contains("-"))
-                {
+                if (state.contains("-")) {
                     sttext.setText("0.0");
-                }
-                else
-                {
+                } else {
                     sttext.setText(state);
                 }
                 changestate = state;
                 showNotification();
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
             }
         });
-        infobtn.setOnClickListener(new View.OnClickListener(){
+        infobtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 팝업 액티비티를 시작하는 코드
@@ -125,6 +135,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("osplog", dataSnapshot.getValue().toString());
                 Weights.setText(state);
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
             }
@@ -138,21 +149,76 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("osblog", dataSnapshot.getValue().toString());
                 Ab.setText(state);
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
             }
         });
 
     }
+
+    private void initSiren() {
+        sirenBox1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sirenBox1.isSelected()){
+                    sirenBox1.setSelected(false);
+                }
+                else{
+                    sirenBox1.setSelected(true);
+                }
+                Toast.makeText(MainActivity.this, "siren", Toast.LENGTH_SHORT).show();
+            }
+        });
+        sirenBox2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sirenBox2.isSelected()){
+                    sirenBox2.setSelected(false);
+                }
+                else{
+                    sirenBox2.setSelected(true);
+                }
+                Toast.makeText(MainActivity.this, "siren", Toast.LENGTH_SHORT).show();
+            }
+        });
+        sirenBox3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sirenBox3.isSelected()){
+                    sirenBox3.setSelected(false);
+                }
+                else{
+                    sirenBox3.setSelected(true);
+                }
+                Toast.makeText(MainActivity.this, "siren", Toast.LENGTH_SHORT).show();
+            }
+        });
+        sirenBox4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sirenBox4.isSelected()){
+                    sirenBox4.setSelected(false);
+                }
+                else{
+                    sirenBox4.setSelected(true);
+                }
+                Toast.makeText(MainActivity.this, "siren", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
     private void showNotification() {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "CHANNEL_ID")
                 .setSmallIcon(R.drawable.ic_notification)
                 .setContentTitle("BOXKEEPER값이 변경됨")
-                .setContentText("BOXKEEPER값이 "+ changestate +"로 변경되었습니다.")
+                .setContentText("BOXKEEPER값이 " + changestate + "로 변경되었습니다.")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         // 알림 표시
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.notify(1, builder.build());
     }
+
 }
