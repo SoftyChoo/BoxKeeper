7af0bad8943694fc6bc1f082bf96f4499ad604aa
추민수 <devsoftychoo@gmail.com>
2023-10-29 05:20:03 +0900

    [Feat] : Common
    1. 휴먼에러를 방지하기 위해 Bundle, Intent등 Key를 관리하기 위한 Common Key Class 구현
    2. 여러 군데서 참조가능하게 하기 위한
    Util Class구현 ex) call

---
+++ boxkeeper_v0.11/app/src/main/java/com/example/boxkeeper/common/Key.java
@@ -0,0 +1,10 @@
+package com.example.boxkeeper.common;
+
+public class Key {
+    public static String INPUT_POSITION = "input_position";
+    public static String INPUT_TYPE = "input_type";
+    public static String INPUT_MODEL = "input_model";
+
+    public static Integer REQUEST_PHONE_CALL = 1;
+
+}
