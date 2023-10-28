61a88fabcff2a69bdf6144642d0808e93356ee3e
추민수 <devsoftychoo@gmail.com>
2023-10-29 05:18:08 +0900

    [Feat] : ViewModel/Factory
    1. 비즈니스 로직을 따로 관리해 확장성과 유지보수성을 올려줄 VIewModel 구현
    2. ViewModel에 의존성을 주입시켜주기 위한 ViewModel Factory 구현

--- boxkeeper_v0.11/app/src/main/java/com/example/boxkeeper/call/CallActivity.java
+++ boxkeeper_v0.11/app/src/main/java/com/example/boxkeeper/call/CallActivity.java
@@ -1,26 +1,65 @@
 package com.example.boxkeeper.call;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
 import com.example.boxkeeper.ListActivity;
+import com.example.boxkeeper.MainActivity;
 import com.example.boxkeeper.R;
 import com.example.boxkeeper.SearchActivity;
 import com.example.boxkeeper.SlideKey;
+import com.example.boxkeeper.common.Key;
+import com.example.boxkeeper.common.Utils;
 import com.example.boxkeeper.databinding.ActivityCallBinding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class CallActivity extends AppCompatActivity {
 
-    private final CallListAdapter listAdapter = new CallListAdapter(item ->
-//            Toast.makeText(getBaseContext(), "item: "+ item, Toast.LENGTH_SHORT).show()
-            Toast.makeText(getBaseContext(), "item: "+ item.getPhoneNumber(), Toast.LENGTH_SHORT).show()
+    private String TAG = "CallActivity";
+
+    private CallSharedViewModel viewModel;
+
+    private CallEditFragment callEditFragment = new CallEditFragment();
+    // ViewModelProvider에 Fragment(this)를 전달하여 ViewModel 생성
+
+    private final CallListAdapter listAdapter = new CallListAdapter(
+            // onClick
+            (item, adapterPosition) -> {
+                if (callEditFragment.isAdded()) {
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction transaction = fragmentManager.beginTransaction();
+                    transaction.remove(callEditFragment);
+                    transaction.commit();
+                    callEditFragment = new CallEditFragment();
+                }
+
+                Bundle args = new Bundle();
+                args.putString(Key.INPUT_TYPE, CallInputType.EDIT.name());
+                args.putInt(Key.INPUT_POSITION, adapterPosition);
+                args.putParcelable(Key.INPUT_MODEL, item);
+                callEditFragment.setArguments(args);
+                callEditFragment.show(getSupportFragmentManager(), "SampleDialog");
+
+                viewModel.setCallItem(item);
+            },
+            // onLongClick
+            item -> {
+                Utils.callPhoneNumber(this, item.getPhoneNumber());
+            }
     );
 
     @Override
@@ -29,24 +68,50 @@ public class CallActivity extends AppCompatActivity {
         ActivityCallBinding binding = ActivityCallBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        viewModel = new ViewModelProvider(this, new CallViewModelFactoty())
+                .get(CallSharedViewModel.class);
+
         initTab(binding);
         initView(binding);
+        initViewModel(binding);
+        listAdapter.submitList(CallSharedViewModel.commonList);
     }
 
-    private void initView(ActivityCallBinding binding){
+    private void initViewModel(ActivityCallBinding binding) {
+        viewModel.callList.observe(this, new Observer<List<CallModel>>() {
+            @Override
+            public void onChanged(List<CallModel> callModels) {
+                Toast.makeText(getBaseContext(), "옵저빙", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "listObserve : " + callModels);
+                listAdapter.submitList(callModels);
+            }
+        });
+
+    }
+
+    private void initView(ActivityCallBinding binding) {
         binding.rvCallList.setLayoutManager(new LinearLayoutManager(this));
         binding.rvCallList.setAdapter(listAdapter);
 
-        listAdapter.submitList(createDummyData());
-    }
-    private List<CallModel> createDummyData() {
-        // 여기에서 더미 데이터를 생성하고 반환합니다.
-        List<CallModel> dummyData = new ArrayList<>();
+        binding.fabCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (callEditFragment.isAdded()) {
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction transaction = fragmentManager.beginTransaction();
+                    transaction.remove(callEditFragment);
+                    transaction.commit();
+                    callEditFragment = new CallEditFragment();
+                }
 
-        for (int i = 1; i <= 10; i++) {
-            dummyData.add(new CallModel("Title " + i, "Description " + i, "PhoneNumber " + i));
-        }
-        return dummyData;
+                Bundle args = new Bundle();
+                args.putString(Key.INPUT_TYPE, CallInputType.ADD.name());
+                callEditFragment.setArguments(args);
+                callEditFragment.show(getSupportFragmentManager(), "SampleDialog");
+
+                viewModel.setCallItem(new CallModel(" ", " ", " "));
+            }
+        });
     }
 
     public void onBackPressed() {
@@ -76,7 +141,7 @@ public class CallActivity extends AppCompatActivity {
         binding.ivCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //
+                listAdapter.submitList(CallSharedViewModel.commonList);
             }
         });
         binding.btnSearchCall.setOnClickListener(new View.OnClickListener() {
@@ -98,4 +163,6 @@ public class CallActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
---
+++ boxkeeper_v0.11/app/src/main/java/com/example/boxkeeper/call/CallSharedViewModel.java
@@ -0,0 +1,68 @@
+package com.example.boxkeeper.call;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CallSharedViewModel extends ViewModel {
+
+    public static List<CallModel> commonList = new ArrayList<>(Arrays.asList(
+            new CallModel("추민수", "상가 건물주", "01012345678"),
+            new CallModel("박수민", "아파트 경비실", "01098765432"),
+            new CallModel("김건희", "아파트 관리사무서", "01022330000")
+    ));
+
+    private String TAG = "CallSharedViewModel";
+
+    public MutableLiveData<List<CallModel>> _callList = new MutableLiveData<>();
+    public LiveData<List<CallModel>> callList = _callList;
+
+    private MutableLiveData<CallModel> _callItem = new MutableLiveData<>();
+    LiveData<CallModel> callItem = _callItem;
+
+//    public CallSharedViewModel() {
+//        // 더미 데이터 생성
+//        List<CallModel> dummyData = new ArrayList<>();
+//        dummyData.add(new CallModel("추민수", "상가 건물주", "111111"));
+//        dummyData.add(new CallModel("박수민", "아파트 경비실", "222222"));
+//        dummyData.add(new CallModel("김건희", "아파트 관리사무서", "333333"));
+//
+//        // MutableLiveData에 더미 데이터 설정
+//        _callList.setValue(dummyData);
+//    }
+
+
+    public void setCallItem(CallModel item) {
+        _callItem.setValue(item);
+    }
+
+    public void addToCallList(CallModel callModel) {
+        Log.d(TAG, "addToCallList callModel num : " + callModel.getPhoneNumber());
+        Log.d(TAG, "addToCallList callModel des : " + callModel.getDescription());
+        Log.d(TAG, "addToCallList callModel title : " + callModel.getTitle());
+        List<CallModel> callList = new ArrayList<>();
+        callList = _callList.getValue(); // _callList 값 가져오기
+        if (callList == null) {
+            callList = new ArrayList<>(); // null일 경우 빈 리스트로 리턴
+        }
+        callList.add(callModel); // item 추가
+        _callList.setValue(callList); // _callList에 데이터 바인딩
+        Log.d(TAG, "addToCallList callList :" + _callList);
+    }
+
+    public void EditToCallList(CallModel callModel, Integer position) {
+        List<CallModel> callList = _callList.getValue(); // _callList 값 가져오기
+
+        if (callList != null && position >= 0 && position < callList.size()) {
+            callList.set(position, callModel); // 지정된 위치(position)에 있는 데이터를 callModel로 변경
+            _callList.setValue(callList); // _callList에 변경된 데이터 바인딩
+        }
+    }
+
+}
